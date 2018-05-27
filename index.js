bc75a2524683418ea5159494ce62fa1ce11d1d7b
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-27 19:29:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var katzDeliLine = [];
 function takeANumber (katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
@@ -12,14 +12,14 @@ function nowServing(katzDeli){
     return "There is nobody waiting to be served!"
 }
 }
+var array = [];
 function currentLine(katzDeli){
-  var line = [];
   for (let i=0; i < katzDeli.length; i++){
-    line.push(` `+[i+1]+`. ` + katzDeli[i])
+    array.push(` `+[i+1]+`. ` + katzDeli[i])
   
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
-  } else return (`The line is currently:` + line);
+  } else return `The line is currently:${array}`;
   
 }
